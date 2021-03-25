009ab1c90fe3d6e959090fb8ceefc70164eb394e
civici <claizerr_@hotmail.com>
2021-03-25 19:48:29 +0300

    new opcodes and methods

--- payload.c
+++ payload.c
@@ -32,9 +32,11 @@ struct Deck* payload_deck(char* payload)
     
 }
 
+struct Deck* currentDeck = NULL;
+
 void payload_check(char* payload, unsigned int payloadbytes)
 {
-    unsigned char opcode = payload[0];
+    unsigned char opcode = *payload;
     unsigned char dataCount = *++payload;
     printf("opcode %d dataCount %d payloadbytes %d\n", opcode, dataCount, payloadbytes);
     switch (opcode)
@@ -42,18 +44,68 @@ void payload_check(char* payload, unsigned int payloadbytes)
         case OPCODE_STRING:
         {
             payload_string(payload);
-            if (dataCount * 2 != payloadbytes - 2)
-                payload_check(++payload + dataCount, payloadbytes - 2 - dataCount);
             break;
         }
         case OPCODE_DECK:
         {
-            struct Deck* deck = payload_deck(payload);
-            gui_printdeck(deck);
-            if (dataCount * 2 != payloadbytes - 2)
-                payload_check(++payload + dataCount * 2, payloadbytes - 2 - dataCount * 2);
+            currentDeck = payload_deck(payload);
+            gui_printdeck(currentDeck);
             break;
         }
-        
+        case OPCODE_GET_CARD:
+        {
+            puts("opcode get card");
+            int index = gui_selectHandler(currentDeck);
+            char outgoingpayload[3] = {4, 1, index};
+            send(connection, outgoingpayload, 3, 0);
+            puts("card index sent");
+            break;
+        }
+        case OPCODE_SENT_CARD:
+        {
+            puts("opcode card");
+            struct Card* c = malloc(sizeof(struct Card));
+            c->koz = *++payload;
+            c->val = *++payload;
+            printf("card is %d %d\n", c->koz, c->val);
+            gui_updatePlayedCardWin(c);
+            break;
+        }
     }
+
+    switch (opcode)
+    {
+        case OPCODE_STRING:
+        {
+            if (dataCount != payloadbytes - 2)
+            {
+                payload_check(++payload + dataCount, payloadbytes - 2 - dataCount);
+            }
+            break;
+        }
+        case OPCODE_DECK:
+        {
+            if (dataCount * 2 != payloadbytes - 2)
+            {
+                payload_check(payload + dataCount * 2 + 1, payloadbytes - 2 - dataCount * 2);
+            }
+            break;
+        }
+        case OPCODE_GET_CARD:
+        {
+            if (dataCount != payloadbytes - 2)
+            {
+                payload_check(++payload, payloadbytes - 2);
+            }
+            break;
+        }
+        case OPCODE_SENT_CARD:
+        {
+            if (dataCount * 2 + 2!= payloadbytes)
+            {
+                payload_check(++payload, payloadbytes - 4);
+            }
+        }
+    }
+
 }
--- payload.h
+++ payload.h
@@ -5,10 +5,13 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "network.h"
 #include "game/Deck.h"
 #include "game/Card.h"
 #include "gui.h"
 
+#define OPCODE_SENT_CARD 5
+#define OPCODE_GET_CARD 3
 #define OPCODE_STRING 2
 #define OPCODE_DECK 1
 
